332be58cc54ebbc8ffc09237201be15fe7ce5403
Arthur Wu <wuqianliang@ismartearth.com>
2018-11-22 13:49:05 +0800

    Update Optimizer.h

--- include/Optimizer.h
+++ include/Optimizer.h
@@ -26,6 +26,7 @@
 #include "KeyFrame.h"
 #include "LoopClosing.h"
 #include "Frame.h"
+#include <Eigen/StdVector>
 
 #include "Thirdparty/g2o/g2o/types/types_seven_dof_expmap.h"
 
@@ -51,7 +52,7 @@ public:
 
     Vector3d static OptimizeInitialGyroBias(const std::list<KeyFrame*> &lLocalKeyFrames);
     Vector3d static OptimizeInitialGyroBias(const std::vector<KeyFrame*> &vLocalKeyFrames);
-    Vector3d static OptimizeInitialGyroBias(const std::vector<Frame> &vFrames);
+    Vector3d static OptimizeInitialGyroBias(const std::vector<Frame, Eigen::aligned_allocator<Frame>> &vFrames);
     Vector3d static OptimizeInitialGyroBias(const vector<cv::Mat>& vTwc, const vector<IMUPreintegrator>& vImuPreInt);
 
     void static LocalBundleAdjustment(KeyFrame *pKF, const std::list<KeyFrame*> &lLocalKeyFrames, bool* pbStopFlag, Map* pMap, LocalMapping* pLM=NULL);
